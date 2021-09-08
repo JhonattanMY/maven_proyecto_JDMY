0e73822eb1bb696fc3b4e52ae6c3c2934a3232ed
Jhonattan Muñoz <jhonattanuchiha2001@gmail.com>
2021-09-08 00:42:39 -0500

    ACTUALIZACION

--- src/main/java1/ejercicios/variables/locales/globales/Animal.java
+++ src/main/java1/ejercicios/variables/locales/globales/Animal.java
@@ -8,9 +8,11 @@ public class Animal {
 	private String nombreCientifico;
 	private String caracteristica;
 	private String habitad;
-	private int edad;
-	private int peso;
-	private String estatura;
+	
+	//VARIABLES GLOBALES ESTATICAS: 
+	private static int edad;
+	private static int peso;
+	private static String estatura;
 
 	// VARIABLES LOCALES: SE DECLARAN A NIVEL DE METODOS
 
@@ -21,4 +23,23 @@ public class Animal {
 		String vitaminas;
 		String postre;
 	}
+	
+//} else if (opcion == 2) {
+//
+//	String datos = (primero.getCedulaEstudiante() + " " + segundo.getCedulaEstudiante() + " "
+//			+ tercero.getCedulaEstudiante() + " " + cuarto.getCedulaEstudiante() + " "
+//			+ quinto.getCedulaEstudiante());
+//
+//	System.out.print("\nIngrese numero de cedula a buscar: ");
+//	String busqueda = lee.nextLine();
+//	boolean resultado = datos.contains(busqueda);
+//
+//	if (resultado == true) {
+//		System.out.println("\n>>>>> Si contiene Estudiante <<<<<\n");
+//	} else
+//		System.out.println("\n>>>>> No contiene Estudiante <<<<<\n");
+//} else {
+//
+//}
+//} while (opcion != 3);
 }
